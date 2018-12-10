c0950d78ae34b315deebbdcd28917db129e7b7bb
Savannah Metheny <methenys@dewv.edu>
2018-12-10 13:43:49 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -1,26 +1,164 @@
-import { game, Sprite } from "./sgc/sgc.js";
-game.setBackground("floor.png");
+import { game, Sprite } from './sgc/sgc.js';
+game.setBackground('floor.png');
 
-class Marker extends Sprite {}
-class PrincessMarker extends Marker {}
-class StrangerMarker extends Marker {}
-class TicTacToe extends Sprite {
-    constructor() {
+class Marker extends Sprite {
+    constructor(board, image, name) {
         super();
-        this.name = "The board";
-        this.setImage("board.png");
-        this.x = 300;
-        this.y = 85;
-        this.squareSize = 150;
-        this.size = 3;
-        this.activeMarker; // variable exists, but value is undefined
+        this.board = board;
+        this.name = 'name';
+        this.squareSymbol = this.name.substring(0, 1);
+        this.setImage(image);
+        this.x = this.startX = 150;
+        this.y = this.startY = 275;
+        this.inBoard = false;
+    }
+    playInSquare(row, col) {
+        this.x = this.board.x + col * this.board.SquareSize + 50;
+        this.y = this.board.y + row * this.board.SquareSize + 50;
+        this.board.dataModel[row][col] = this.squareSymbol;
+        this.board.debugBoard();
+        this.inBoard = true;
+    }
+}
+class PrincessMarker extends Marker {
+    constructor(board) {
+        super(board, 'annFace.png', 'Princess Ann');
+        this.dragging = false;
 
     }
-    takeTurns() {
-        this.activeMarker = new PrincessMarker(this);
+    handleMouseLeftButtonDown() {
+        if (this.inBoard) return;
+        this.dragging = true;
+    }
+    handleMouseLeftButtonUp() {
+        if (this.inBoard) return;
+        this.dragging = false;
+        let row = Math.floor((this.y - this.board.y) / 150);
+        let col = Math.floor((this.x - this.board.x) / 150);
+        if (row < 0 || row > 2 || col < 0 || col > 2 ||
+            this.board.dataModel[row][col] !== this.board.emptySquareSymbol) {
+            this.y = this.startY;
+            this.x = this.startX;
+            return true;
+        }
+        this.playInSquare(row, col);
+        this.board.takeTurns();
+    }
+    handleGameLoop() {
+        if (this.dragging) {
+            this.x = game.getMouseX() - this.width / 2;
+            this.y = game.getMouseY() - this.height / 2;
+        }
+
+
     }
 
 }
+class StrangerMarker extends Marker {
+    constructor(board) {
+        super(board, 'strangerFace.png', 'Stranger');
 
+    }
+    handleGameLoop() {
+        if (this.inBoard) {
+            console.log(this.inBoard);
+            return true;
+
+        }
+        console.log("akdfhlsdkjfhs");
+        // Mark a random empty square.
+        let row, col;
+        do {
+            row = Math.round(Math.random() * (this.board.size - 1));
+            col = Math.round(Math.random() * (this.board.size - 1));
+        } while (this.board.dataModel[row][col] !== this.board.emptySquareSymbol);
+        this.board.dataModel[row][col] = this.squareSymbol;
+        console.log(row, col);
+        this.playInSquare(row, col);
+        this.board.takeTurns();
+
+    }
+}
+class TicTacToe extends Sprite {
+    constructor() {
+        super();
+        this.name = 'The board';
+        this.setImage('board.png');
+        this.x = 300;
+        this.y = 85;
+        this.SquareSize = 150;
+        this.size = 3;
+        this.activeMarker; // variable exists, but value is undefined
+        this.emptySquareSymbol = "-";
+        this.dataModel = [];
+        for (let row = 0; row < this.size; row = row + 1) {
+            this.dataModel[row] = [];
+            for (let col = 0; col < this.size; col = col + 1) {
+                this.dataModel[row][col] = this.emptySquareSymbol;
+            }
+        }
+
+    }
+
+    debugBoard() {
+        let boardString = '\n';
+        let moveCount = 0;
+        for (let row = 0; row < this.size; row = row + 1) {
+            for (let col = 0; col < this.size; col = col + 1) {
+                boardString = boardString + this.dataModel[row][col] + ' ';
+                if (this.dataModel[row][col] !== this.emptySquareSymbol) moveCount = moveCount + 1;
+            }
+            boardString = boardString + '\n';
+        }
+        console.log('The current state of the board is ' + moveCount + 'move(s)' + boardString);
+    }
+    takeTurns() {
+        if (this.gameIsWon()) {
+            let message = '        Game Over.\n        ';
+            if (this.activeMarker instanceof PrincessMarker) {
+                message = message + 'The Princess wins.';
+            }
+            else if (this.activeMarker instanceof StrangerMarker) {
+                message = message + 'The Stranger wins.';
+            }
+            game.end(message);
+            return;
+        }
+
+        if (this.gameIsDrawn()) {
+            game.end('        Game Over.\n        The game ends in a draw.');
+            return;
+        }
+        if (!this.activeMarker) {
+            if (Math.random() < 0.5) this.activeMarker = new PrincessMarker(this);
+            else this.activeMarker = new StrangerMarker(this);
+        }
+        else if (this.activeMarker instanceof PrincessMarker) {
+            // princess has moved; now it's stranger's turn
+            this.activeMarker = new StrangerMarker(this);
+        }
+        else if (this.activeMarker instanceof StrangerMarker) {
+            // stranger has moved; now it's princess's turn
+            this.activeMarker = new PrincessMarker(this);
+        }
+    }
+    gameIsWom() {
+        // Are there three of the same markers diagonally from upper left?
+        if (this.board[0][0] === this.board[1][1] &&
+            this.board[1][1] === this.board[2][2] &&
+            this.board[2][2] !== this.emptySquareSymbol
+        ) {
+            return true;
+        }
+    }
+
+    markSquare(row, col, forOpponent) {}
+    unmarkSquare(row, col) {}
+    getSquareSymbol(row, col) {}
+    gameIsWon() {}
+    gameIsDrawn() {}
+    countWinningMoves(forOpponent) {}
+
+}
 let theBoard = new TicTacToe();
 theBoard.takeTurns();
